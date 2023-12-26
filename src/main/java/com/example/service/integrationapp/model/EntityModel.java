c7e9fbcd561efddb8eff39acce9b7fb39a5af028
Aleksey <kolchenkoav@gmail.com>
2023-12-26 22:10:01 +0300

    first commit

---
+++ src/main/java/com/example/service/integrationapp/model/EntityModel.java
@@ -0,0 +1,21 @@
+package com.example.service.integrationapp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class EntityModel {
+    private UUID id;
+    private String name;
+    private Instant date;
+
+    public static EntityModel createMockModel(String name) {
+        return new EntityModel(UUID.randomUUID(), name, Instant.now());
+    }
+}
