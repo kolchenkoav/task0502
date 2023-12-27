524c34ea7270d7937f38b012e962fc168f5c61c6
Aleksey <kolchenkoav@gmail.com>
2023-12-27 18:57:01 +0300

    OkHttpClientSender ++

---
+++ src/main/java/com/example/service/integrationapp/controller/EntityClientController.java
@@ -0,0 +1,48 @@
+package com.example.service.integrationapp.controller;
+
+import com.example.service.integrationapp.clients.OkHttpClientSender;
+import com.example.service.integrationapp.clients.RestTemplateClient;
+import com.example.service.integrationapp.model.EntityModel;
+import com.example.service.integrationapp.model.UpsertEntityRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/client/entity")
+@RequiredArgsConstructor
+public class EntityClientController {
+    //private final OkHttpClientSender client;
+    private final RestTemplateClient client;
+
+    @GetMapping
+    public ResponseEntity<List<EntityModel>> entityList() {
+        return ResponseEntity.ok(client.getEntityList());
+    }
+
+    @GetMapping("/{name}")
+    public ResponseEntity<EntityModel> entityByName(@PathVariable String name) {
+        return ResponseEntity.ok(client.getEntityByName(name));
+    }
+
+    @PostMapping
+    public ResponseEntity<EntityModel> createEntity(@RequestBody UpsertEntityRequest request) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(client.createEntity(request));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<EntityModel> updateEntity(@PathVariable UUID id, @RequestBody UpsertEntityRequest request) {
+        return ResponseEntity.ok(client.updateEntity(id, request));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<EntityModel> deleteEntityById(@PathVariable UUID id) {
+        client.deleteEntityById(id);
+        return ResponseEntity.noContent().build();
+    }
+    //22:17
+}
---
+++ src/main/java/com/example/service/integrationapp/controller/FileClientController.java
@@ -0,0 +1,38 @@
+package com.example.service.integrationapp.controller;
+
+import com.example.service.integrationapp.clients.OkHttpClientSender;
+import lombok.RequiredArgsConstructor;
+
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/v1/client/file")
+@RequiredArgsConstructor
+public class FileClientController {
+    private final OkHttpClientSender client;
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> uploadFile(@RequestPart MultipartFile file) {
+        client.uploadFile(file);
+
+        return ResponseEntity.ok("File was upload");
+    }
+
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<Resource> downloadFile(@PathVariable String filename) {
+        Resource resource = client.downloadFile(filename);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + filename);
+        headers.setContentType(MediaType.TEXT_PLAIN);
+
+        return ResponseEntity.ok()
+                .headers(headers)
+                .body(resource);
+    }
+}
