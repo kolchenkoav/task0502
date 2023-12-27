24c882799510dcc37c50de1a5859d1d1057c3b98
Aleksey <kolchenkoav@gmail.com>
2023-12-27 19:03:51 +0300

    OkHttpClientSender +++

--- src/main/java/com/example/service/integrationapp/configuration/ClientConfiguration.java
+++ src/main/java/com/example/service/integrationapp/configuration/ClientConfiguration.java
@@ -1,9 +1,11 @@
 package com.example.service.integrationapp.configuration;
 
 import okhttp3.OkHttpClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ClientConfiguration {
@@ -15,5 +17,12 @@ public class ClientConfiguration {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @Bean
+    public WebClient webClient(@Value("${app.integration.base-url}") String baseUrl) {
+        return WebClient.builder()
+                .baseUrl(baseUrl)
+                .build();
     }
 }
